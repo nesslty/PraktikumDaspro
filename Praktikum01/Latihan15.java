d44d7344bc19c841599c9b9fb053998522f9fac6
nesslty <pineapplekuu@gmail.com>
2024-09-06 09:38:39 +0700

    commit praktikum01

---
+++ Praktikum01/Latihan15.java
@@ -0,0 +1,14 @@
+/**
+ * Latihan15
+ */
+public class Latihan15 {
+
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama    : Nety Sulistyorini");
+        System.out.println("Absen   : 15");
+        System.out.println("Kelas   : SIB-1D");
+        System.out.println("Prodi   : D-IV Sistem Informasi Bisnis");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
