ce45c96254a08139e761b398d3bee2c8ec2b635c
nesslty <pineapplekuu@gmail.com>
2024-09-17 10:01:33 +0700

    ini Praktikum03

--- Praktikum03/ContohTipeData15.java
+++ Praktikum03/ContohTipeData15.java
@@ -9,10 +9,10 @@ public class ContohTipeData15 {
         long saldo = 150000000;
         int angkaDesimal = 0x10;
 
-        System.out.println("Golongan darah\t: " + (byte)golonganDarah);
+        System.out.println("Golongan darah\t: " + golonganDarah);
         System.out.println("Jumlah Penduduk\t: " + jumlahPenduduk);
         System.out.println("Suhu\t\t: " + suhu);
-        System.out.println("Berat\t\t: " + (float)berat);
+        System.out.println("Berat\t\t: " + berat);
         System.out.println("Saldo\t\t: " + saldo);
         System.out.println("Angka desimal\t: " + angkaDesimal);
     }
--- Praktikum03/ContohVariabel15.java
+++ Praktikum03/ContohVariabel15.java
@@ -3,15 +3,15 @@ package Praktikum03;
 public class ContohVariabel15 {
     public static void main(String[] args) {
        String salahSatuHobiSayaAdalah = "Bermain petak umpet";
-       boolean isPandai = true;
-       char jeniskelamin = 'P';
-       byte _umurSayaSekarang = 19;
+       boolean isPandai = false;
+       char jenisKelamin = 'P';
+       byte umurSayaSekarang = 19;
        double $ipk = 3.24, tinggi = 1.68;
        
        System.out.println(salahSatuHobiSayaAdalah);
        System.out.println("Apakah pandai? " + isPandai);
-       System.out.println("Jenis Kelamin: " + jeniskelamin);
+       System.out.println("Jenis Kelamin: " + jenisKelamin);
        System.out.println("IPK: " + $ipk);
-       System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+       System.out.println(String.format("Saya berumur %s dengan tinggi badan %s ", umurSayaSekarang, tinggi));
     } 
 }
