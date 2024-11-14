716a9258830fe58f16f9ea665e2b70340a3af97c
nesslty <pineapplekuu@gmail.com>
2024-11-14 17:00:36 +0700

    terbaru

--- Praktikum09/ArrayBilangan15.java
+++ Praktikum09/ArrayBilangan15.java
@@ -2,13 +2,20 @@ package Praktikum09;
 
 public class ArrayBilangan15 {
     public static void main(String[] args) {
-        int[] bil = {5, 13, -7, 17};
+        // int[] bil = {5, 13, -7, 17};
+        int[] bil = new int[4];
 
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+        for (int i = 0; i < 4; i++)
+        System.out.println(bil[i]);
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        // System.out.println(bil[0]);
+        // System.out.println(bil[1]);
+        // System.out.println(bil[2]);
+        // System.out.println(bil[3]);
 
     }
 }
