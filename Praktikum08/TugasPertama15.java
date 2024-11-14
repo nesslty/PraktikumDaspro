a460386dbea7c3d2421093392f9ce287884bbbfc
nesslty <pineapplekuu@gmail.com>
2024-11-14 16:45:35 +0700

    terbaru

--- Praktikum08/TugasPertama15.java
+++ Praktikum08/TugasPertama15.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class TugasPertama15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
 
