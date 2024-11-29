cdd687c2fa5b5730ba9fc17e4d6607d9c7745354
nesslty <pineapplekuu@gmail.com>
2024-11-29 12:43:25 +0700

    terbaru

--- Praktikum11/PengunjungCafe15.java
+++ Praktikum11/PengunjungCafe15.java
@@ -1,8 +1,10 @@
 public class PengunjungCafe15 {
     public static void daftarPengunjung (String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+
+        // for (int i = 0; i < namaPengunjung.length; i++) {
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
     public static void main(String[] args) {
