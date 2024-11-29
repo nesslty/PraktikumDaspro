c15fc1abca544b3c1c00285f0fd859f53bdfb8bc
nesslty <pineapplekuu@gmail.com>
2024-11-29 15:25:32 +0700

    terbaru

--- Praktikum11/kafe215.java
+++ Praktikum11/kafe215.java
@@ -1,8 +1,16 @@
 public class kafe215 {
-    public static void Menu (String namaPelanggan, boolean isMember) {
+    public static void Menu (String namaPelanggan, boolean isMember, String promo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% di setiap pembelian!");
+        }
+
+        if (promo.equalsIgnoreCase("DISKON5O")) {
+            System.out.println("anda mendapat diskon 50%");
+        } else if (promo.equalsIgnoreCase("DISKON30")) {
+            System.out.println("anda mendapat diskon 30%");
+        } else {
+            System.out.println("kode invalid!!");
         }
         System.out.println("===== [ MENU RESTO KAFE ] =====");
         System.out.println("1. Kopi Hitam   = Rp 15,000");
@@ -15,6 +23,7 @@ public class kafe215 {
         System.out.println("Silahkan pilih menu yang anda inginkan.");
     }
     public static void main(String[] args) { 
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON50");
         }
 }
+
